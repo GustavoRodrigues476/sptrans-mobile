f7b0aed081531e450f16ceb63f0e8af98310237e
Gustavo Raimundo Rodrigues <gustavo.raimundo.rodrigues@gmail.com>
2026-03-30 19:25:07 -0300

    feat: add splash screen

--- frontend/mobilidade_urbana_app/lib/main.dart
+++ frontend/mobilidade_urbana_app/lib/main.dart
@@ -9,12 +9,109 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello teste!'),
-        ),
-      ),
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false
     );
   }
 }
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    const Center(child: Text('Início')),
+    const Center(child: Text('Ir')),
+    const Center(child: Text('Linhas')),
+    const Center(child: Text('Conta'))
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/icon/icon.png',
+          height: 64,
+        ),
+      ),
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        unselectedLabelStyle: const TextStyle(
+          color: Colors.grey,
+        ),
+        selectedLabelStyle: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        ),
+        backgroundColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              decoration: BoxDecoration(
+                color: currentIndex == 0 ? Colors.lightGreenAccent : Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.home),
+            ),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              decoration: BoxDecoration(
+                color: currentIndex == 1 ? Colors.lightGreenAccent : Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.directions_bus_outlined),
+            ),
+            label: 'Ir',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              decoration: BoxDecoration(
+                color: currentIndex == 2 ? Colors.lightGreenAccent : Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.merge_outlined),
+            ),
+            label: 'Linhas',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              decoration: BoxDecoration(
+                color: currentIndex == 3 ? Colors.lightGreenAccent : Colors.transparent,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Icon(Icons.account_circle_outlined),
+            ),
+            label: 'Perfil',
+          ),
+        ],
+      )
+    );
+  }
+}
+
